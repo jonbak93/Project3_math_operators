10e0c403763439680d0238058ddc232d534767d7
jonbak93 <48571275+jonbak93@users.noreply.github.com>
2020-11-16 13:01:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
  function addBy() {
      num1 = document.getElementById("firstNumber").value;
      num2 = document.getElementById("secondNumber").value;
-     document.getElementById("result").innerHTML = +num1 + +num2;
+     document.getElementById("result").innerHTML = +num1 + +num2; //adding two digits together to get the sum
  }
 
 
@@ -19,7 +19,7 @@
  function subtractBy() {
      num1 = document.getElementById("firstNumber").value;
      num2 = document.getElementById("secondNumber").value;
-     document.getElementById("result").innerHTML = num1 - num2;
+     document.getElementById("result").innerHTML = num1 - num2; //subtracting two numbers together to get the difference
  }
 
 
@@ -27,27 +27,27 @@
  function multiplyBy() {
      num1 = document.getElementById("firstNumber").value;
      num2 = document.getElementById("secondNumber").value;
-     document.getElementById("result").innerHTML = num1 * num2;
+     document.getElementById("result").innerHTML = num1 * num2; //multiplying two numbers together to get the value
  }
 
 
  function divideBy() {
      num1 = document.getElementById("firstNumber").value;
      num2 = document.getElementById("secondNumber").value;
-     document.getElementById("result").innerHTML = num1 / num2;
+     document.getElementById("result").innerHTML = num1 / num2; //dividing two numbers to get the value 
  }
 
 
  function modulus() {
      num1 = document.getElementById("firstNumber").value;
      num2 = document.getElementById("secondNumber").value;
-     document.getElementById("result").innerHTML = num1 % num2;
+     document.getElementById("result").innerHTML = num1 % num2; //using the modulus operator to get the remainder
  }
 
 
  function negation() {
      var x = 10;
-     document.getElementById("result").write = -x;
+     document.getElementById("result").write = -x; //using the (-) symbol to get the opposite value
 
  }
 
@@ -57,7 +57,7 @@
  function increment() {
      var increase = 7;
      increase++;
-     document.getElementById("increment").innerHTML = increase;
+     document.getElementById("increment").innerHTML = increase; //using the (++) operator to get the value +1 of whatever number I put in
  }
 
  increment();
@@ -65,7 +65,7 @@
  function decrement() {
      var decrease = 7;
      decrease--;
-     document.getElementById("decrement").innerHTML = decrease;
+     document.getElementById("decrement").innerHTML = decrease; //using the (--) operator to get the value -1 of whatever number I put in
  }
 
  decrement();
